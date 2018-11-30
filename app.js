926404563c1349fe95a02697e86dfe12a53bc534
abell95 <belland@oregonstate.edu>
2018-11-29 22:28:32 -0800

    All functionality in place, but nslookup is returning the same ip each time

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const { exec } = require("child_process");
 const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const PORT = 5000;
@@ -12,18 +13,22 @@ app.get("/", (req, res) => {
 });
 
 app.post("/post_data", (req, res) => {
-  const newPost = Object.keys(req.body);
-  console.log(newPost);
-  res.status(200).send();
+  const url = req.body.url;
+  if (!url) {
+    res.status(400).send();
+    return;
+  }
+  exec(`nslookup ${url}`, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      console.log("stderr: ", stderr);
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(JSON.stringify({ data: stdout }));
+  });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  exec("nslookup www.google.com", (err, stdout, stderr) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("stdout: ", stdout);
-  });
 });
--- readme.md
+++ readme.md
@@ -4,6 +4,9 @@ Andrew Bell - Code project for CS 372
 
 This site allows you to run nslookup online.
 You probably could just use it by command line, but you might be lazy or not know how.
+In such a case, I pity you and implore you to use my site.
+
+The input expects a full URL - i.e. `https://www.example.com`, it will not permit something like `www.youtube.com` or just `google.com`
 
 ## Steps for installation and running:
 
