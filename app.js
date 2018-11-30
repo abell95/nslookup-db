22a789f518a1e20126b0c0b511d3b7c680567027
abell95 <belland@oregonstate.edu>
2018-11-29 16:02:04 -0800

    Reworked project concept, deleted old iddea

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
+const { exec } = require("child_process");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 const PORT = 5000;
 
@@ -8,22 +11,19 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/fetch_data", (req, res) => {
-  let data = fs.readFileSync(__dirname + "/posts.json", "utf8");
-  res.status(200).send(data);
-});
-
 app.post("/post_data", (req, res) => {
-  const newPost = req.query;
-  console.log(newPost.title);
-  let rawData = fs.readFileSync(__dirname + "/posts.json", "utf8");
-  let posts = JSON.parse(rawData);
-  console.log(posts);
-  posts[newPost.title] = newPost.text;
-  fs.writeFileSync(__dirname + "/posts.json");
-  res.status(201).send(posts);
+  const newPost = Object.keys(req.query);
+  console.log(newPost);
+  res.status(200).send();
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  exec("nslookup www.google.com", (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("stdout: ", stdout);
+  });
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # nslookup-db
 
-Code project for CS 372
+Andrew Bell - Code project for CS 372
+
+## Steps for installation:
+
+-- Make sure Node is installed
+-- Clone to local machine
+-- Navigate to dir and run `npm install` in command line
+-- Run `node app`
